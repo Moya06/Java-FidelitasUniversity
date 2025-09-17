c8393af46c612fde0de3d3330524a78cf6d03788
Moya06 <fabianmoya353@gmail.com>
2025-09-16 19:41:31 -0600

    second commit

---
+++ Repository/src/main/java/com/mycompany/repository/Repository.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repository;
+
+/**
+ *
+ * @author Fabián
+ */
+public class Repository {
+
+    public static void main(String[] args) {
+        int numero = 0;
+        System.out.print(numero);
+    }
+}
